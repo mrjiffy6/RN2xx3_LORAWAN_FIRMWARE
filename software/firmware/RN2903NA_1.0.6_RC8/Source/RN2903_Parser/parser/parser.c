6e7229cd6b5f753675e686d0ba6f1d563f6db4b1
Nicolas Juteau <juteaun@amotus.ca>
2021-11-05 11:19:22 -0400

    - added custom tag to firmware version string

--- software/firmware/RN2903NA_1.0.6_RC8/Source/RN2903_Parser/parser/parser.c
+++ software/firmware/RN2903NA_1.0.6_RC8/Source/RN2903_Parser/parser/parser.c
@@ -44,9 +44,9 @@
 #include "../mcc_generated_files/eusart1.h"
 
 #if defined(DEBUGMACMSG) || defined(DEBUGLINKADR)
-#define VER_STR            "1.0.6 RC6-test1-debug"
+#define VER_STR            "1.0.6 RC8-custom-test1-debug"
 #else
-#define VER_STR            "1.0.6 RC8"
+#define VER_STR            "1.0.6 RC8-custom"
 #endif // DEBUG...
 #define HW_STR             "RN2903"
 
